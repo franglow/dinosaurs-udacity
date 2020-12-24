7cf42f35a561d038d9fc0f191651fdb033d4666c
Fran <elfrancortez@gmail.com>
2020-12-24 13:06:23 +0100

    Misseing compare methods added. Randomize dinos facts added. Remove form added.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Create Dino Constructor
+    // Create Dino Constructor
     function Dino(species, weight, height, diet, where, when, fact) {
         this.species = species;
         this.weight = weight;
@@ -18,11 +18,11 @@
             return new Dino(species, weight, height, diet, where, when, fact )
         });
         generateTile(dinos_array);
-      }
+    }
     
       
 
-    // Create Dino Objects  
+    // Create Dino Object
     const dino = new Dino();    
 
     // Create Human Object
@@ -37,25 +37,20 @@
 
     // Use IIFE to get human data from form
     const getHumanData = (function () {
-        let species;
-        let height;
-        let weight;
-        let diet;
         function getData() {
-            species = document.querySelector('#name').value;
-            height= parseInt(document.querySelector('#feet').value) * 12 + parseInt(document.querySelector('#inches').value);
-            weight= document.querySelector('#weight').value;
-            diet= document.querySelector('#diet').value;
-            return new Human(species, height, weight, diet);    
+            human.species = document.querySelector('#name').value;
+            human.height= parseInt(document.querySelector('#feet').value) * 12 + parseInt(document.querySelector('#inches').value);
+            human.weight= document.querySelector('#weight').value;
+            human.diet= document.querySelector('#diet').value.toLowerCase();
         }
         return {
-            human: getData
+            human: getData,
         }
     })();
     
     // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches. 
-    Dino.prototype.compareWeight = function (fact) {
+    Dino.prototype.compareWeight = (fact) => {
         if (dino.weight > human.weight) {
             dino.fact = `${dino.species} 
                 is ${dino.weight - human.weight} lbs heavier than ${human.species}`;
@@ -69,50 +64,99 @@
     
     
     // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
+    Dino.prototype.compareHeight = (fact) => {
+        if (dino.weight > human.weight) {
+            dino.fact = `${dino.species} 
+                is ${dino.weight - human.weight} inches taller than ${human.species}`;
+            return dino.fact;    
+        } else {
+            dino.fact = `${dino.species} 
+                is ${human.weight - dino.weight} inches smaller than ${human.species}`;
+            return dino.fact;    
+        }
+    }
     
     
     // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-    
+    Dino.prototype.compareDiet = (fact) => {
+        if (human.diet === dino.diet) {
+            dino.fact = `${dino.species} 
+                is ${dino.diet} like ${human.species}`;
+            return dino.fact;    
+        } else {
+            dino.fact = `${dino.species} 
+                is ${dino.diet} but ${human.species} is ${human.diet} `;
+            return dino.fact;    
+        }
+    }
+
+
     
     // Generate Tiles for each Dino in Array
     const generateTile = (dinos_array) => {
         let update_dino = [];
-        dinos_array.forEach(dino_array_item => {
-            dino.species = dino_array_item.species
-            dino.height = dino_array_item.height
-            dino.weight = dino_array_item.weight
-            dino.diet = dino_array_item.diet
-            // if (dino.species === 'Pigeon') {
-            //     dino.fact = "All birds are living dinosaurs."
-            // } else {
+
+        // Workaround for a fixed dinos_array length
+        const sifter_array = [1,1,1,0,0,0,0];
+        // Shuffle sifter array to later use it to randomize compare methods.
+        shuffle(sifter_array);
+        //Getting Pigeon position and adding to sifter_array in order to keep it fact property.
+        let pigeon_index = dinos_array.findIndex(dino => dino.species === 'Pigeon');
+        sifter_array.splice(pigeon_index, 0, 0);
+
+        dinos_array.forEach((dinos_array_item, i) => {
+
+            // Assign fetched array properties to global array properties.
+            dino.species = dinos_array_item.species
+            dino.height = dinos_array_item.height
+            dino.weight = dinos_array_item.weight
+            dino.diet = dinos_array_item.diet
+            if (sifter_array[i]) {
                 let get_random_number = Math.floor(Math.random() * 3) + 1;
-                if (dino_array_item instanceof Human) {
+                if (dinos_array_item instanceof Human) {
                     get_random_number = '';
                 } 
                 switch (get_random_number) {
                     case 1:
-                        dino.compareWeight(dino_array_item.fact);
+                        dino.compareHeight(dinos_array_item.fact);
                         break;
                     case 2:
-                        dino.compareWeight(dino_array_item.fact);
+                        dino.compareWeight(dinos_array_item.fact);
                         break;
                     case 3:
-                        dino.compareWeight(dino_array_item.fact);
+                        dino.compareDiet(dinos_array_item.fact);
                         break;
                     default:
                         break;
                 } 
-            // }
+            } else {
+                dino.fact = dinos_array_item.fact;
+            }
+
             update_dino.push(JSON.parse(JSON.stringify(dino)));
         });
-        // Adding human object in the middle to place in the center
+
+        // Adding human object in the middle
         update_dino.splice(4, 0, human);
         update_dino.forEach((dino_element) => {
             addTileToDOM(dino_element);
         });
+    }
+
+    /**
+    * Shuffles array in place.
+    * @param {Array} An array containing the items.
+    * https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+    */
+    function shuffle(a) {
+       var j, x, i;
+       for (i = a.length - 1; i > 0; i--) {
+           j = Math.floor(Math.random() * (i + 1));
+           x = a[i];
+           a[i] = a[j];
+           a[j] = x;
+       }
+       return a;
     }
     
 
@@ -126,7 +170,6 @@
         const img_node = document.createElement("img");
         const p_node = document.createElement("p");
 
-
         if (dino_element instanceof Human) {
             img_node.src = "./images/human.png"; 
         } else {
@@ -135,7 +178,7 @@
         } 
         h3_node.textContent = dino_element.species;
         p_node.textContent = dino_element.fact;
-        //append DOM element div under the grid
+
         div.appendChild(h3_node);
         div.appendChild(img_node);
         div.appendChild(p_node);
@@ -145,23 +188,17 @@
 
    
  // Remove form from screen
- function removeForm(e) {
+ function removeForm() {
     const form = document.querySelector('#dino-compare');
     form.style.display = "none";
-    this.reset();
   }
  
  
  // On button click, prepare and display infographic
-function compare(e) {
+ function compare() {
     getDinoJson();
     getHumanData.human();
-    removeForm(e);
-    
+    removeForm();
 }
-
- const compare_button = document.querySelector('#btn');
-    compare_button.addEventListener('click', compare);
-
-
- 
+const compare_button = document.querySelector('#btn');
+compare_button.addEventListener('click', compare); 
